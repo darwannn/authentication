a451187ff5166738b425c9c30521f91c3d343610
Darwin Ramos <darwin.ramos.sl@bulsu.edu.ph>
2024-02-22 10:04:10 +0800

    add scheduler

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -13,6 +13,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('app:daily')->everyMinute()
+            ->appendOutputTo(storage_path('logs/daily.log'));
+    }
+
+    public function scheduleTimezone()
+    {
+        return 'UTC';
     }
 
     /**
@@ -20,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
